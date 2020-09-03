0e3fc97eb5c76c4e851afca99ef5a925f4182d9b
Lucien Greathouse <me@lpghatguy.com>
2020-09-02 23:15:09 -0700

    Update crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,14 @@
 [![thunderdome on crates.io](https://img.shields.io/crates/v/thunderdome.svg)](https://crates.io/crates/thunderdome)
 [![thunderdome docs](https://img.shields.io/badge/docs-docs.rs-orange.svg)](https://docs.rs/thunderdome)
 
-Generational arena inspired by
+Thunderdome is a ~gladitorial~ generational arena inspired by
 [generational-arena](https://crates.io/crates/generational-arena),
 [slotmap](https://crates.io/crates/slotmap), and
-[slab](https://crates.io/crates/slab).
+[slab](https://crates.io/crates/slab). It provides constant time insertion,
+lookup, and removal via small (8 byte) keys returned from `Arena`.
+
+Thunderdome's key type, `Index`, is still 8 bytes when put inside of an
+`Option<T>` thanks to Rust's `NonZero*` types.
 
 ## Basic Examples
 
@@ -44,6 +48,7 @@ assert_eq!(arena.get(foo), None);
 | Non-`Copy` Values            | Yes         | Yes                | Sorta¹  | Yes  |
 | no-std support               | No          | Yes                | No      | No   |
 | Serde support                | No          | Yes                | Yes     | No   |
+| [Immune to ABA Problem][ABA] | Yes         | Yes                | Yes     | No   |
 
 * Sizes calculated on rustc `1.44.0-x86_64-pc-windows-msvc`
 * See [the Thunderdome comparison
@@ -59,6 +64,8 @@ assert_eq!(arena.get(foo), None);
 Thunderdome supports Rust 1.34.1 and newer. Until Thunderdome reaches 1.0,
 changes to the MSRV will require major version bumps. After 1.0, MSRV changes
 will only require minor version bumps, but will need significant justification.
+
+[ABA]: https://en.wikipedia.org/wiki/ABA_problem
 */
 
 #![forbid(missing_docs)]
