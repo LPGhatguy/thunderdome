700ba896ae9af22c1bcec0c001fe4e293c317b21
Lucien Greathouse <me@lpghatguy.com>
2026-08-03 10:15:31 -0400

    Also implement `DoubleEndedIterator` for `IterMut`

--- src/iter/iter_mut.rs
+++ src/iter/iter_mut.rs
@@ -47,6 +47,36 @@ impl<'a, T> Iterator for IterMut<'a, T> {
     }
 }
 
+impl<'a, T> DoubleEndedIterator for IterMut<'a, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.len == 0 {
+                return None;
+            }
+
+            match self.inner.next_back()? {
+                (_, Entry::Empty(_)) => (),
+                (slot, Entry::Occupied(occupied)) => {
+                    self.len = self.len.checked_sub(1).unwrap_or_else(|| {
+                        unreachable!("Underflowed u32 trying to iterate Arena in reverse")
+                    });
+
+                    let slot = slot
+                        .try_into()
+                        .unwrap_or_else(|_| unreachable!("Overflowed u32 trying to iterate Arena"));
+
+                    let index = Index {
+                        slot,
+                        generation: occupied.generation,
+                    };
+
+                    return Some((index, &mut occupied.value));
+                }
+            }
+        }
+    }
+}
+
 impl<'a, T> FusedIterator for IterMut<'a, T> {}
 impl<'a, T> ExactSizeIterator for IterMut<'a, T> {}
 
@@ -78,5 +108,63 @@ mod test {
 
         assert!(pairs.contains(&(one, &mut 1)));
         assert!(pairs.contains(&(two, &mut 2)));
+    }
+
+    #[test]
+    fn iter_rev() {
+        let mut arena = Arena::with_capacity(2);
+        let one = arena.insert(1);
+        let two = arena.insert(2);
+
+        let mut pairs = HashSet::new();
+        let mut iter = arena.iter_mut().rev();
+        assert_eq!(iter.size_hint(), (2, Some(2)));
+
+        pairs.insert(iter.next().unwrap());
+        assert_eq!(iter.size_hint(), (1, Some(1)));
+
+        pairs.insert(iter.next().unwrap());
+        assert_eq!(iter.size_hint(), (0, Some(0)));
+
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.size_hint(), (0, Some(0)));
+
+        assert!(pairs.contains(&(two, &mut 2)));
+        assert!(pairs.contains(&(one, &mut 1)));
+    }
+
+    #[test]
+    fn iter_both_directions() {
+        let mut arena = Arena::with_capacity(2);
+        let one = arena.insert(1);
+        let two = arena.insert(2);
+        let three = arena.insert(3);
+        let four = arena.insert(4);
+
+        let mut pairs = HashSet::new();
+        let mut iter = arena.iter_mut();
+        assert_eq!(iter.size_hint(), (4, Some(4)));
+
+        pairs.insert(iter.next().unwrap());
+        assert_eq!(iter.size_hint(), (3, Some(3)));
+
+        pairs.insert(iter.next_back().unwrap());
+        assert_eq!(iter.size_hint(), (2, Some(2)));
+
+        pairs.insert(iter.next_back().unwrap());
+        assert_eq!(iter.size_hint(), (1, Some(1)));
+
+        pairs.insert(iter.next().unwrap());
+        assert_eq!(iter.size_hint(), (0, Some(0)));
+
+        assert_eq!(iter.next_back(), None);
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.size_hint(), (0, Some(0)));
+
+        assert!(pairs.contains(&(one, &mut 1)));
+        assert!(pairs.contains(&(two, &mut 2)));
+        assert!(pairs.contains(&(three, &mut 3)));
+        assert!(pairs.contains(&(four, &mut 4)));
     }
 }
